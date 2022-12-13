8630c4aa41960729227dbd1da8855a0099a74465
Vlad <vlad_zaycev_1984@mail.ru>
2022-12-13 23:15:15 +0300

    убрал пустые строки

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -1,6 +1,5 @@
 ﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 длина которых меньше 3 символа*/
-
 string[] array1 = new string[6] {"yes", "5", "!", "Brains", "01234", "Geek"};
 string[] array2 = new string[array1.Length];
 SecondArray(array1, array2);
@@ -16,7 +15,6 @@ void SecondArray(string[] array1, string[] array2)
         count++;
     }
 }
-
 PrintArray(array2);
 void PrintArray(string[] array)
 {
